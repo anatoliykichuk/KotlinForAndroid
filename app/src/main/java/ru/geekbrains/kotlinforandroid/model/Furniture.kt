8cebe21614ec06f51d60c43eeab578f740415ce8
Анатолий Кичук <anatoliy.kichuk@gmail.com>
2021-10-02 10:45:19 +0300

    Добавлены элементы структуры данных

--- app/src/main/java/ru/geekbrains/kotlinforandroid/model/Furniture.kt
+++ app/src/main/java/ru/geekbrains/kotlinforandroid/model/Furniture.kt
@@ -2,10 +2,6 @@ package ru.geekbrains.kotlinforandroid.model
 
 import java.util.*
 
-interface Furniture {
-    val name: String
-    val description: String
-    val kind: KindOfFurniture
-    val style: StyleOfFurniture
-    val dateOfManufacture: Date
-}
+class Furniture(val name: String, val description: String,
+    val kind: KindOfFurniture, val style: StyleOfFurniture,
+    val dateOfManufacture: Date)
